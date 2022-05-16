bb594abdbdb965680819039ec5367840c18d0977
ArturDokhno <artur.dohno@icloud.com>
2022-05-16 19:00:57 +0500

    Initial Commit

---
+++ PinMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinMap
+//
+//  Created by Артур Дохно on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
