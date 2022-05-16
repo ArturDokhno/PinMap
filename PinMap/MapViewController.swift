0307ddba2aa6d8201b99c9f9ce98800f9740874e
ArturDokhno <artur.dohno@icloud.com>
2022-05-16 19:42:01 +0500

    Добавление трех кнопок на экран

--- PinMap/MapViewController.swift
+++ PinMap/MapViewController.swift
@@ -16,11 +16,48 @@ class MapViewController: UIViewController {
         mapView.translatesAutoresizingMaskIntoConstraints = false
         return mapView
     }()
-
+    
+    let addAddressButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("ADD ADDRESS", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let routeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Route", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Reset", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setConstraints()
+        
+        addAddressButton.addTarget(self, action: #selector(addAddressButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(addAddressButtonTapped), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func addAddressButtonTapped() {
+        print("Tap add")
+    }
+    
+    @objc func routeButtonTapped() {
+        print("Tap route")
+    }
+    
+    @objc func resetButtonTapped() {
+        print("Tap reset")
     }
     
 }
@@ -34,6 +71,24 @@ extension MapViewController {
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
+        ])
+        
+        mapView.addSubview(addAddressButton)
+        NSLayoutConstraint.activate([
+            addAddressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 70),
+            addAddressButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20)
+        ])
+        
+        mapView.addSubview(routeButton)
+        NSLayoutConstraint.activate([
+            routeButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -70),
+            routeButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20)
+        ])
+        
+        mapView.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -70),
+            resetButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20)
         ])
     }
 }
